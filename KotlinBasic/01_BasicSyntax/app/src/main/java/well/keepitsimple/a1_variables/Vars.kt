90ff3291cca467d34a3bf58629cc55fbdfeb8b33
Skillbox <it@skillbox.ru>
2021-06-26 11:28:50 +0000

    Add lesson

---
+++ KotlinBasic/01_BasicSyntax/app/src/main/java/well/keepitsimple/a1_variables/Vars.kt
@@ -0,0 +1,24 @@
+package well.keepitsimple.a1_variables
+
+fun main() {
+    val firstName: String = "Vladislav"
+    val lastName: String = "Vladimirov"
+    var height: Int = 192
+    var weight: Float = 60F
+
+    var isChild: Boolean = height<150 && weight<40F
+
+    var info = "$firstName $lastName $height $weight $isChild"
+
+    println(info)
+
+    height = 100
+
+    weight = 20F
+
+    isChild = height<150 && weight<40F
+
+    info = "$firstName $lastName $height $weight $isChild"
+
+    println(info)
+}
