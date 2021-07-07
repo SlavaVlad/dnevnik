a4baba5a6dca2dff22344ed6edf5e1c6f72482d2
Vlad <nauka.2.0.vs@gmail.com>
2021-07-07 11:30:12 +0300

    Д/з скорректировано (d >= 0)

--- KotlinBasic/02_Functions/app/src/main/java/well/keepitsimple/functions/Functions.kt
+++ KotlinBasic/02_Functions/app/src/main/java/well/keepitsimple/functions/Functions.kt
@@ -14,16 +14,16 @@ fun solveEquation(a: Int, b: Int, c: Int): Double {
 
     var discriminant = (b * b) - (4 * a * c) // вычисление дискриминанта
 
-        if (discriminant > 0) {
+        if (discriminant >= 0) {
 
             var x1: Double = (-b + sqrt(discriminant.toDouble())) / (2 * a) // первый корень
 
             var x2: Double = (-b - sqrt(discriminant.toDouble())) / (2 * a) // второй корень
 
-//            println("""Дискриминант: $discriminant
-//x1 = $x1
-//x2 = $x2""")
-// для отладки, ибо пункты останова для слабаков
+                println("""Дискриминант: $discriminant
+    x1 = $x1
+    x2 = $x2""")
+    //для отладки, ибо пункты останова для слабаков
 
             return x1 + x2
 
