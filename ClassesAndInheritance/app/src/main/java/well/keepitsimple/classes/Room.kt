776fbd9002c2d1ceda651269e56506058df84d8a
SlavaVlad <nauka.2.0.vs@gmail.com>
2021-08-04 14:00:28 +0300

    HW #5 created, class Room created.

--- ClassesAndInheritance/app/src/main/java/well/keepitsimple/classes/Room.kt
+++ ClassesAndInheritance/app/src/main/java/well/keepitsimple/classes/Room.kt
@@ -1,4 +1,17 @@
 package well.keepitsimple.classes
 
-class Room {
+class Room(_area: Double){
+
+    public val area: Double = _area
+
+    open protected var title = "Обычная комната"
+
+    fun getDescription(): String{
+        return "Название: $title, Площадь: $area"
+    }
+
+}
+
+class Person(_name: String){
+    public val name: String = _name
 }
