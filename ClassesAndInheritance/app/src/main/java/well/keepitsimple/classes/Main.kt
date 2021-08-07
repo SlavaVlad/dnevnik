686b395ad871513b3a993af12d09ccb897cb59ec
SlavaVlad <nauka.2.0.vs@gmail.com>
2021-08-07 13:46:47 +0300

    HW #5, completed.

--- ClassesAndInheritance/app/src/main/java/well/keepitsimple/classes/Main.kt
+++ ClassesAndInheritance/app/src/main/java/well/keepitsimple/classes/Main.kt
@@ -3,6 +3,20 @@ package well.keepitsimple.classes
 import java.util.*
 
 fun main() {
-    val Primary = Room(50.0)
-    println(Primary.getDescription())
+    val room = Room(50.0)
+    val BedRoom       = BedRoom(15.0)
+    val LivingRoom    = LivingRoom(15.0)
+    val Kitchen       = Kitchen(15.0)
+    val Bathroom      = Bathroom(15.0)
+    val ChildrensRoom = ChildrensRoom(15.0)
+    val Main          = Main(15.0, "Main room")
+    println(room.getDescription() + "\n" +
+            BedRoom.getDescription() + "\n" +
+            LivingRoom.getDescription() + "\n" +
+            Kitchen.getDescription() + "\n" +
+            Bathroom.getDescription() + "\n" +
+            ChildrensRoom.getDescription() + "\n" +
+            Main.getDescription()
+    )
 }
+
--- ClassesAndInheritance/app/src/main/java/well/keepitsimple/classes/Room.kt
+++ ClassesAndInheritance/app/src/main/java/well/keepitsimple/classes/Room.kt
@@ -1,10 +1,10 @@
 package well.keepitsimple.classes
 
-class Room(_area: Double){
+open class Room(_area: Double){
 
-    public val area: Double = _area
+    open public val area: Double = _area
 
-    open protected var title = "Обычная комната"
+    open protected val title = "Обычная комната"
 
     fun getDescription(): String{
         return "Название: $title, Площадь: $area"
@@ -12,6 +12,12 @@ class Room(_area: Double){
 
 }
 
-class Person(_name: String){
-    public val name: String = _name
-}
+class BedRoom      (_area: Double) : Room(_area) { override val title = "Bed room"; override val area = _area }
+class LivingRoom   (_area: Double) : Room(_area) { override val title = "Living room"; override val area = _area }
+class Kitchen      (_area: Double) : Room(_area) { override val title = "Kitchen"; override val area = _area }
+class Bathroom     (_area: Double) : Room(_area) { override val title = "Bathroom"; override val area = _area }
+class ChildrensRoom(_area: Double) : Room(_area) { override var title = "Children's room"; override val area = _area }
+class Main         (_area: Double, _title: String) : Room(_area) { override var title = _title; override val area = _area }
+//class MyRoom(_area: Double, _title: String) : Room(_area) { override val title = _title; override val area = _area }
+
+//Создайте комнаты с характеристиками жилища своей мечты. Выведите информацию о получившихся комнатах на экран.
